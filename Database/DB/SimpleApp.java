96027425934993fa432ba30160ab85c6cdbe5a0f
joshuacrawford <joshuacrawford552@gmail.com>
2017-04-25 17:25:35 -0500

    DB

---
+++ Database/DB/SimpleApp.java
@@ -0,0 +1,201 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import java.util.ArrayList;
+import java.util.Properties;
+
+public class CreateTables
+{
+    private String framework = "embedded";
+    private String protocol = "jdbc:derby:";
+
+    public static void main(String[] args)
+    {
+        new SimpleApp().go(args);
+        System.out.println("Tables Created");
+    }
+	
+    void go(String[] args)
+    {
+        parseArguments(args);
+
+        System.out.println("SimpleApp starting in " + framework + " mode");
+
+        Connection conn = null;
+        ArrayList<Statement> statements = new ArrayList<Statement>(); // list of Statements, PreparedStatements
+        PreparedStatement psInsert;
+        PreparedStatement psUpdate;
+        Statement s;
+        ResultSet rs = null;
+        try
+        {
+            Properties props = new Properties(); 
+            props.put("user", "user1");
+            props.put("password", "user1");
+
+            String dbName = "Asset_Tracking";
+
+            conn = DriverManager.getConnection(protocol + dbName
+                    + ";create=true", props);
+            conn.setAutoCommit(false);
+
+            s = conn.createStatement();
+            statements.add(s);
+			
+            s.execute("create table Asset(asset_id int, asset_des varchar(100))");
+			s.execute("create table Location(loc_id int, loc_des varchar(100))");
+			s.execute("create table Tracking(asset_id int, loc_id int"));
+
+            conn.commit();
+            System.out.println("Committed the transaction");
+
+            /*
+             * In embedded mode, an application should shut down the database.
+             * If the application fails to shut down the database,
+             * Derby will not perform a checkpoint when the JVM shuts down.
+             * This means that it will take longer to boot (connect to) the
+             * database the next time, because Derby needs to perform a recovery
+             * operation.
+             *
+             * It is also possible to shut down the Derby system/engine, which
+             * automatically shuts down all booted databases.
+             *
+             * Explicitly shutting down the database or the Derby engine with
+             * the connection URL is preferred. This style of shutdown will
+             * always throw an SQLException.
+             *
+             * Not shutting down when in a client environment, see method
+             * Javadoc.
+             */
+
+            if (framework.equals("embedded"))
+            {
+                try
+                {
+                    // the shutdown=true attribute shuts down Derby
+                    DriverManager.getConnection("jdbc:derby:;shutdown=true");
+
+                    // To shut down a specific database only, but keep the
+                    // engine running (for example for connecting to other
+                    // databases), specify a database in the connection URL:
+                    //DriverManager.getConnection("jdbc:derby:" + dbName + ";shutdown=true");
+                }
+                catch (SQLException se)
+                {
+                    if (( (se.getErrorCode() == 50000)
+                            && ("XJ015".equals(se.getSQLState()) ))) {
+                        // we got the expected exception
+                        System.out.println("Derby shut down normally");
+                        // Note that for single database shutdown, the expected
+                        // SQL state is "08006", and the error code is 45000.
+                    } else {
+                        // if the error code or SQLState is different, we have
+                        // an unexpected exception (shutdown failed)
+                        System.err.println("Derby did not shut down normally");
+                        printSQLException(se);
+                    }
+                }
+            }
+        }
+        catch (SQLException sqle)
+        {
+            printSQLException(sqle);
+        } finally {
+            // release all open resources to avoid unnecessary memory usage
+
+            // ResultSet
+            try {
+                if (rs != null) {
+                    rs.close();
+                    rs = null;
+                }
+            } catch (SQLException sqle) {
+                printSQLException(sqle);
+            }
+
+            // Statements and PreparedStatements
+            int i = 0;
+            while (!statements.isEmpty()) {
+                // PreparedStatement extend Statement
+                Statement st = (Statement)statements.remove(i);
+                try {
+                    if (st != null) {
+                        st.close();
+                        st = null;
+                    }
+                } catch (SQLException sqle) {
+                    printSQLException(sqle);
+                }
+            }
+
+            //Connection
+            try {
+                if (conn != null) {
+                    conn.close();
+                    conn = null;
+                }
+            } catch (SQLException sqle) {
+                printSQLException(sqle);
+            }
+        }
+    }
+
+    /**
+     * Reports a data verification failure to System.err with the given message.
+     *
+     * @param message A message describing what failed.
+     */
+    private void reportFailure(String message) {
+        System.err.println("\nData verification failed:");
+        System.err.println('\t' + message);
+    }
+
+    /**
+     * Prints details of an SQLException chain to <code>System.err</code>.
+     * Details included are SQL State, Error code, Exception message.
+     *
+     * @param e the SQLException from which to print details.
+     */
+    public static void printSQLException(SQLException e)
+    {
+        // Unwraps the entire exception chain to unveil the real cause of the
+        // Exception.
+        while (e != null)
+        {
+            System.err.println("\n----- SQLException -----");
+            System.err.println("  SQL State:  " + e.getSQLState());
+            System.err.println("  Error Code: " + e.getErrorCode());
+            System.err.println("  Message:    " + e.getMessage());
+            // for stack traces, refer to derby.log or uncomment this:
+            //e.printStackTrace(System.err);
+            e = e.getNextException();
+        }
+    }
+
+    /**
+     * Parses the arguments given and sets the values of this class's instance
+     * variables accordingly - that is, which framework to use, the name of the
+     * JDBC driver class, and which connection protocol to use. The
+     * protocol should be used as part of the JDBC URL when connecting to Derby.
+     * <p>
+     * If the argument is "embedded" or invalid, this method will not change
+     * anything, meaning that the default values will be used.</p>
+     * <p>
+     * @param args JDBC connection framework, either "embedded" or "derbyclient".
+     * Only the first argument will be considered, the rest will be ignored.
+     */
+    private void parseArguments(String[] args)
+    {
+        if (args.length > 0) {
+            if (args[0].equalsIgnoreCase("derbyclient"))
+            {
+                framework = "derbyclient";
+                protocol = "jdbc:derby://localhost:1527/";
+            }
+        }
+    }
+}
