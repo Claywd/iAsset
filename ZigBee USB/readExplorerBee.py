43136774e8386ccb40bae6874662a8bb77de3af9
Clayton Dittman <claywdittman@icloud.com>
2017-04-26 18:45:21 -0500

    fixed the script to filter duplicate reads

--- ZigBee USB/readExplorerBee.py
+++ ZigBee USB/readExplorerBee.py
@@ -1,12 +1,12 @@
-import serial, time, String
+import serial, time
 
 ser = serial.Serial('/dev/cu.usbserial-DN02SMAF', 9600)
-serial_line = ser.readline()
+serialLine = ser.readline()
 while 1:
-
-    if (ser.readline() != serial_line)
-        serial_line = ser.readline()
-        print(serial_line) # If using Python 2.x use: print serial_line
+    serialNextLine = ser.readline()
+    if serialNextLine != serialLine:
+        serialLine = serialNextLine
+        print(serialLine) # If using Python 2.x use: print serial_line
     # Do some other work on the data
     time.sleep(2) # sleep 5 minutes
 
